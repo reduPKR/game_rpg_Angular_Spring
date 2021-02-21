9011b46be4ce706bf47030f17562533e4f1c43c9
redu <reduardo19942@gmail.com>
2021-02-21 17:50:40 -0300

    descricao da raça adicionada ao front end

--- game-app/src/app/shared/model/race.module.ts
+++ game-app/src/app/shared/model/race.module.ts
@@ -3,5 +3,6 @@ import { SkillPoints } from "./skill-points.model";
 export class Race { 
   id: number = 0;
   name: string = "";
+  description: string = "";
   skill_points: SkillPoints | undefined;
 }
